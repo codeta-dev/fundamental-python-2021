91f7c5a67e5d9a50a801d91770ec426ed735cd6c
Irwan Alamsyah <irwanalamsyah.dev@gmail.com>
2021-02-09 02:39:52 +0900

    pembuatan package internal, penggunaan package external dengan requests, fundamental 2 selesai

---
+++ CrashCourse_Part2/tahap_2/geometry/persegipanjang.py
@@ -0,0 +1,5 @@
+def hitung_luas_persegipanjang(panjang, lebar):
+    return panjang*lebar
+
+def info_persegipanjang():
+    return "Modul menghitung luas persegi panjang"
---
+++ CrashCourse_Part2/tahap_2/use_geometry.py
@@ -0,0 +1,9 @@
+# Berkenalan Dengan Packge
+from geometry.segitiga import info_segitiga, hitung_luas_segitiga
+from geometry.persegipanjang import info_persegipanjang, hitung_luas_persegipanjang
+
+print(info_segitiga())
+print(f"Hitung luas segitiga {hitung_luas_segitiga(10, 3)}")
+
+print(info_persegipanjang())
+print(f"Hitung luas persegi panjang {hitung_luas_persegipanjang(10, 3)}")
